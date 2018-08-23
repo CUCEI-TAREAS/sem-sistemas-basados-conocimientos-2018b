b9ffc957fb39550a9e58c76807efe5f4adc3de06
Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>
2018-08-23 09:53:58 -0500

    Add practica 01
    Signed-off-by: Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>

---
+++ practicas/01/SSPSBC_P1_FELIPE_DE_JESUS_RUIZ_GARCIA_214522077.pl
@@ -0,0 +1,22 @@
+gustanPerros(adrian).
+gustanPerros(fernando).
+gustanPerros(maria).
+
+gustanGatos(angel).
+gustanGatos(monica).
+gustanGatos(alejandro).
+
+gustanPajaros(luis).
+gustanPajaros(gustavo).
+gustanPajaros(miguel).
+
+odiaGatos(miguel).
+
+tieneGatos(maria).
+tieneGatos(luis).
+tienePerros(miguel).
+
+tienePerros(luis).
+
+esGato(bichito, alejandro).
+
